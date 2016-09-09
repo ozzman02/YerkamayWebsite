5fba8eae2605392ef1925d4136afffcdcd7c0f75
ozzman02 <osantamaria@gmail.com>
2016-09-09 14:28:53 -0600

    Creating project
    Project created

---
+++ YerkamayWebsite/src/main/java/com/yerkamay/web/YerkamayWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.yerkamay.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerkamayWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerkamayWebsiteApplication.class, args);
+	}
+}
