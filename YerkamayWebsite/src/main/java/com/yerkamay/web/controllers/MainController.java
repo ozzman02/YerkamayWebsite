f9ea98cd0711076e084a195742b50214cd53e856
ozzman02 <osantamaria@gmail.com>
2016-09-12 12:34:11 -0600

    Working on HTML: becas, centro, directora and terapias

--- YerkamayWebsite/src/main/java/com/yerkamay/web/controllers/MainController.java
+++ YerkamayWebsite/src/main/java/com/yerkamay/web/controllers/MainController.java
@@ -12,4 +12,94 @@ public class MainController {
 		return "index";
 	}
 	
+	@RequestMapping("/centro")
+	public String getCentro(ModelAndView modelAndView) {
+		return "centro";
+	}
+	
+	@RequestMapping("/directora")
+	public String getDirectora(ModelAndView modelAndView) {
+		return "directora";
+	}
+	
+	@RequestMapping("/tpsicologicas")
+	public String getTerapiasPsicologicas(ModelAndView modelAndView) {
+		return "tpsicologicas";
+	}
+	
+	@RequestMapping("/tinfantil")
+	public String getTerapiaInfantil(ModelAndView modelAndView) {
+		return "tinfantil";
+	}
+	
+	@RequestMapping("/tadolescentes")
+	public String getTerapiaAdolescentes(ModelAndView modelAndView) {
+		return "tadolescentes";
+	}
+	
+	@RequestMapping("/tespiritual")
+	public String getTerapiaEspiritual(ModelAndView modelAndView) {
+		return "tespiritual";
+	}
+	
+	@RequestMapping("/tgapoyo")
+	public String getTerapiaGruposApoyo(ModelAndView modelAndView) {
+		return "tgapoyo";
+	}
+	
+	@RequestMapping("/taromaterapia")
+	public String getTerapiaAromaterapiaRelajacion(ModelAndView modelAndView) {
+		return "taromaterapia";
+	}
+	
+	@RequestMapping("/tpsicosomaticas")
+	public String getTerapiasPsicosomaticas(ModelAndView modelAndView) {
+		return "tpsicosomaticas";
+	}
+
+	@RequestMapping("/talleres")
+	public String getTalleres(ModelAndView modelAndView) {
+		return "talleres";
+	}
+	
+	@RequestMapping("/capacitaciones")
+	public String getCapacitaciones(ModelAndView modelAndView) {
+		return "capacitaciones";
+	}
+	
+	@RequestMapping("/coaching")
+	public String getCoaching(ModelAndView modelAndView) {
+		return "coaching";
+	}
+	
+	@RequestMapping("/csocial")
+	public String getCompromisoSocial(ModelAndView modelAndView) {
+		return "csocial";
+	}
+	
+	@RequestMapping("/csaludintegral")
+	public String getSaludIntegral(ModelAndView modelAndView) {
+		return "saludintegral";
+	}
+	
+	@RequestMapping("/donaciones")
+	public String getDonaciones(ModelAndView modelAndView) {
+		return "donaciones";
+	}
+	
+	@RequestMapping("/testimonios")
+	public String getTestimonios(ModelAndView modelAndView) {
+		return "testimonios";
+	}
+	
+	@RequestMapping("/contacto")
+	public String getContacto(ModelAndView modelAndView) {
+		return "contacto";
+	}
+	
+	@RequestMapping("/becas")
+	public String getBecas(ModelAndView modelAndView) {
+		return "becas";
+	}
+	
 }
